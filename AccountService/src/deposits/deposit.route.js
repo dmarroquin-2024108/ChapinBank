2f48870e6f67d8ab9fd735aa83220670a9295217
elopez-2021393 <elopez-2021393@kinal.edu.gt>
2026-04-22 20:54:50 -0600

    fix(deposits): Arreglar funcionalidad de reversión de depósitos

--- AccountService/src/deposits/deposit.route.js
+++ AccountService/src/deposits/deposit.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { createDeposit } from './deposit.controller.js';
-import { validateCreateDeposit } from '../../middlewares/deposit-validator.js';
+import { createDeposit, revertDeposit } from './deposit.controller.js';
+import { validateCreateDeposit, validateRevertDeposit } from '../../middlewares/deposit-validator.js';
 
 const router = Router();
 
@@ -8,6 +8,12 @@ router.post(
     '/',
     validateCreateDeposit,
     createDeposit
+);
+
+router.patch(
+    '/:id/revert',
+    validateRevertDeposit,
+    revertDeposit
 );
 
 export default router;
