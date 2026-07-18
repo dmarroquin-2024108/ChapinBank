8b1475d135155562ad479f6334a91a9f670adfa0
dmarroquin-2024108 <dmarroquin-2024108@kinal.edu.gt>
2026-07-18 01:13:14 -0600

    feat(vercel): Agregar configuracion para desplegar en vercel

--- AccountService/configs/cors.configuration.js
+++ AccountService/configs/cors.configuration.js
@@ -1,6 +1,6 @@
 export const corsOptions = {
   origin: true,
-  credential: true,
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
