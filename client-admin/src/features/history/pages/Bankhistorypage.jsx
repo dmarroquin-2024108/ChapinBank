1902e7e02afd53bbb9fd5d8c8a8887b61e72c247
dmarroquin-2024108 <dmarroquin-2024108@kinal.edu.gt>
2026-07-18 01:27:43 -0600

    fix(history): Renombrar los archivos de los imports a su nombre original

--- client-admin/src/features/history/pages/Bankhistorypage.jsx
+++ client-admin/src/features/history/pages/Bankhistorypage.jsx
@@ -9,7 +9,7 @@ import {
   Wallet,
   PiggyBank,
 } from 'lucide-react';
-import { useHistoryStore } from '../store/Historystore.js';
+import { useHistoryStore } from '../store/historyStore.js';
 import { MovementCard } from '../components/Movementcard.jsx';
 import { getAccountsList } from '../../../shared/apis/admin.js';
 
